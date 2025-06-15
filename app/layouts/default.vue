58a1ab6ceb382b2dbc3cdc803fe00d6728866e79
Michael John P. Arevalo <michaelarevaloooo@gmail.com>
2025-06-15 17:39:43 +0800

    feat:implement dark mode toggle logic in default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,31 @@
 <template>
-	<v-app>
-		<AppNavbar/>
-		<v-main>
-			<slot />
+	<v-app :class="darkMode ? 'dark-theme' : ''" >
+		<AppNavbar :darkMode="darkMode" @toggle-theme="toggleTheme" />
+		<v-main class="d-flex flex-column fill-height">
+			<v-container class="flex-grow-0">
+				<slot />
+			</v-container>
 		</v-main>
-		<v-footer app color="primary" dark>
-      		<v-col class="text-center">© 2025 SpaceX Fan App</v-col>
-    	</v-footer>
+		<AppFooter/>
 	</v-app>
 </template>
 <script>
 export default {
 	name: 'DefaultLayout',
 }
+
+
+const darkMode = ref(false)
+
+const toggleTheme = () => {
+  darkMode.value = !darkMode.value
+  localStorage.setItem('darkMode', darkMode.value)
+}
+
+onMounted(() => {
+  if (localStorage.getItem('darkMode') === 'true') {
+    darkMode.value = true
+  }
+})
 </script>
+
